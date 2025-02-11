16cdd882a1cecc2dbec3a3195e5ef566973f7987
riyen-perdana <riyenperdana@uin-suska.ac.id>
2025-02-11 12:54:52 +0700

    commit

---
+++ app/Filament/Pages/Registration.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Form;
+use Filament\Pages\Auth\Register;
+use Illuminate\Support\HtmlString;
+use Filament\Forms\Components\Wizard;
+use Illuminate\Support\Facades\Blade;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Contracts\Support\Htmlable;
+
+class Registration extends Register
+{
+    protected ?string $maxWidth = '4xl';
+    protected static ?string $getTitle = 'test';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Wizard::make([
+                    Wizard\Step::make('Data Pribadi')
+                        ->schema([
+                            TextInput::make('agm_nama')
+                                ->label('Agama')
+                        ]),
+                    Wizard\Step::make('step2')
+                        ->schema([
+                            TextInput::make('agm_nama')
+                        ]),
+                    Wizard\Step::make('step3')
+                        ->schema([
+                            TextInput::make('agm_nama')
+                        ]),
+                ])
+            ]);
+    }
+
+    protected function getFormActions(): array
+    {
+        return [];
+    }
+
+    public function getHeading(): string | Htmlable
+    {
+        return 'Silahkan Isi Data Dan Berkas Anda';
+    }
+
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdmincpanelPanelProvider::class,
+    App\Providers\Filament\PpgPanelProvider::class,
 ];
