ffd4476915904d62c544e3c1e9946c5b37c6751c
riyenperdana <riyenperdana@uin-suska.ac.id>
2025-02-17 16:27:20 +0700

    commit

--- app/Filament/Resources/PpgResource.php
+++ app/Filament/Resources/PpgResource.php
@@ -12,15 +12,19 @@ use Faker\Provider\ar_EG\Text;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Illuminate\Support\HtmlString;
+use Filament\Support\Enums\Alignment;
 use Filament\Infolists\Components\Grid;
 use Filament\Infolists\Components\Tabs;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Infolists\Components\Split;
 use Filament\Notifications\Notification;
+use Filament\Forms\Components\FileUpload;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use App\Filament\Resources\PpgResource\Pages;
 use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Actions\Action;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\PpgResource\RelationManagers;
 
@@ -148,31 +152,202 @@ class PpgResource extends Resource
         return $infolist
             ->schema([
                 Grid::make()
-                ->columns([
-                    'sm' => 1,
-                    'xl' => 1,
-                    '2xl' => 1,
-                ])
-
-                ->schema([
-                    Tabs::make()
-                        ->tabs([
-                            Tabs\Tab::make('Data Peserta')
-                                ->schema([
-                                    ImageEntry::make('ppg_foto')
-                                        ->label('')
-                                        ->circular(),
-                                    schema([
-                                        TextEntry::make('ppg_nama')
-                                            ->label('Nama Peserta'),
-                                        TextEntry::make('ppg_nama')
-                                            ->label('Nama Peserta'),
+                    ->columns([
+                        'sm' => 1,
+                        'xl' => 1,
+                        '2xl' => 1,
+                    ]),
+                    
+                    Section::make('Data Peserta PPG Universitas Islam Negeri Sultan Syarif Kasim Riau')
+                    ->footerActions([
+                        Action::make('Verifikasi')
+                            ->action(function () {
+                                // ...
+                            }),
+                    ])
+                    ->footerActionsAlignment(Alignment::End)
+                    ->schema([
+                        Tabs::make('Tabs')
+                            ->tabs([
+                                Tabs\Tab::make('Data Pribadi')
+                                    ->schema([
+                                        Section::make('')
+                                            ->schema([
+                                                ImageEntry::make('ppg_foto')
+                                                    ->label('')
+                                                    ->square()
+                                                    ->width(176)
+                                                    ->height(236)
+                                                    ->alignCenter(),
+                                                Section::make('')
+                                                    ->schema([
+                                                        TextEntry::make('ppg_nama')
+                                                            ->label('Nama Peserta')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_nik')
+                                                            ->label('Nomor Induk Kependudukan')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_simpatika')
+                                                            ->label('Akun SIMPATIKA')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_nisn')
+                                                            ->label('Nomor Induk Siswa Nasional (NISN)')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_email')
+                                                            ->label('Email')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_jk')
+                                                            ->label('Jenis Kelamin')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('agm.agm_nama')
+                                                            ->label('Agama')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_tpt_lhr')
+                                                            ->label('Tempat Lahir')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_tgl_lhr')
+                                                            ->label('Tempat Lahir')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_ibu')
+                                                            ->label('Nama Ibu Kandung')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('kec.kkot.prov.prov_nama')
+                                                            ->label('Provinsi Asal')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('kec.kkot.kkot_nama')
+                                                            ->label('Kabupaten Kota Asal')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('kec.kec_nama')
+                                                            ->label('Kecamatan Asal')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                        TextEntry::make('ppg_no_wa')
+                                                            ->label('No Whatsapp')
+                                                            ->extraAttributes([
+                                                                'class' => 'font-bold -mt-4',
+                                                            ]),
+                                                    ])
+                                                    ->columns(2)
+                                            ])
+                                    ]),
+                                Tabs\Tab::make('Sekolah dan Universitas')
+                                    ->schema([
+                                        Section::make('')
+                                            ->schema([
+                                                TextEntry::make('ppg_sklh')
+                                                    ->label('Sekolah Asal')
+                                                    ->extraAttributes([
+                                                        'class' => 'font-bold -mt-4',
+                                                    ]),
+                                                TextEntry::make('ppg_no_ops')
+                                                    ->label('No Op Sekolah')
+                                                    ->extraAttributes([
+                                                        'class' => 'font-bold -mt-4',
+                                                    ]),
+                                                TextEntry::make('prdp.unv.unv_nama')
+                                                    ->label('Nama Universitas Asal')
+                                                    ->extraAttributes([
+                                                        'class' => 'font-bold -mt-4',
+                                                    ]),
+                                                TextEntry::make('prdp.prdp_nama')
+                                                    ->label('Program Studi Asal')
+                                                    ->extraAttributes([
+                                                        'class' => 'font-bold -mt-4',
+                                                    ]),
+                                                TextEntry::make('ppg_ipk')
+                                                    ->label('IPK')
+                                                    ->extraAttributes([
+                                                        'class' => 'font-bold -mt-4',
+                                                    ])
+                                            ])
+                                    ]),
+                                Tabs\Tab::make('Data Unggah RPL')
+                                    ->schema([
+                                        TextEntry::make('ppg_ijz')
+                                            ->html()
+                                            ->label('File Ijazah')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_ijz, shouldOpenInNewTab: true),
+                                        TextEntry::make('ppg_trsk')
+                                            ->html()
+                                            ->label('File Transkrip Nilai')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_trsk, shouldOpenInNewTab: true),
+                                        TextEntry::make('ppg_sk_ajr')
+                                            ->html()
+                                            ->label('File SK Mengajar')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_sk_ajr, shouldOpenInNewTab: true),
+                                        TextEntry::make('ppg_prkt_ajr')
+                                            ->html()
+                                            ->label('File Perangkat Mengajar')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_prkt_ajr, shouldOpenInNewTab: true),
+                                        TextEntry::make('ppg_strf')
+                                            ->html()
+                                            ->label('File Sertifikat')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_strf, shouldOpenInNewTab: true),
+                                        TextEntry::make('ppg_dkmn')
+                                            ->html()
+                                            ->label('File Dokumen Ajar')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_dkmn, shouldOpenInNewTab: true),
+                                        TextEntry::make('ppg_invs')
+                                            ->html()
+                                            ->label('File Inovasi Mengajar')
+                                            ->state('Unduh File')
+                                            ->extraAttributes([
+                                                'class' => 'font-bold -mt-3',
+                                            ])
+                                            ->url(fn(Ppg $record): string => '/storage/' . $record->ppg_invs, shouldOpenInNewTab: true),
                                     ])
                                     ->columns(2),
-                                ])
-                                ->columns(2)
-                        ])
-                ])
+                            ])        
+                    ])
             ]);
     }
 }
